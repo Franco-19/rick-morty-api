9abb3338e4bec06c8c7c6578dab4ba0c3fafbb38
Franco-19 <jarafranco122@gmail.com>
2021-09-08 13:40:12 -0300

    1 file modified

--- src/components/badgeElement/BadgeElement.js
+++ src/components/badgeElement/BadgeElement.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 /** Styles */
-import './BadgeElement.styles.css'
+import './BadgeElement.styles.css';
 
 const BadgeElement = () => {
   return (
-    <div>
-      <p className="badge-title">BadgeElement</p>
+    <div className="badge-container">
+      <p className="badge-container__title">Nombre personaje</p>
+      <button>ver mas</button>
     </div>
-  )
-}
+  );
+};
 
-export default BadgeElement
+export default BadgeElement;
